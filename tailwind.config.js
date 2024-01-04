dd188691342a5902044d973aaedd8bdddd07bb6f
Maurice Knoll <maurice.knoll@adesso.de>
2024-01-04 22:18:33 +0100

    Add new colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,21 @@ export default {
             },
             background: {
                 'flyff-example': 'url("/storage/images/flyff-example.jpeg")',
-            }
+            },
+            colors: {
+                'cerise': '#dd4a6cff',
+                'charcoal': '#1c4052ff',
+                'robin-blue': '#4fc5d5ff',
+                'coral': '#fe906bff',
+                'acri-magenta': '#852a4bff',
+                'old-rose': '#c67e70ff',
+                'cordovan': '#9a4650ff',
+                'oxford-blue': '#001b30ff',
+                'mantis': '#50b95eff',
+                'platinum': '#dbe1dfff',
+            },
         },
+
     },
 
     plugins: [forms, typography],
